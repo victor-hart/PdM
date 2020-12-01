f89449936f51bdb63b6fc7eb693c0d4054513984
forvis <cuong.edu3012@gmail.com>
2020-12-01 13:35:47 +0400

    commit old version of PdM

--- inst/shinyPdM/ui.R
+++ inst/shinyPdM/ui.R
@@ -3,6 +3,7 @@ library(shiny)
 library(shinythemes)
 library(ggplot2)
 library(caret)
+library(DT)
 
 # Define UI for PdM app
 ui <- tagList(
@@ -13,7 +14,29 @@ ui <- tagList(
   # App navbar ----
   navbarPage("PdM",
     # Load data from different sources (local csv file, SQL,..) ---
-    tabPanel("Data Import", icon = icon("folder-open")),
+    tabPanel("Data Import", icon = icon("folder-open"),
+             br(),
+             fluidRow(column(3,
+                             wellPanel(
+                               h4(strong("Data Import")),
+                               selectInput("source", label="Select Data Source",
+                                           choices = list("MySQL" = "mysql",
+                                                          "Local file" = "local",
+                                                          "Source 3" = "Source 3"
+                                           ),
+                                           multiple=F),
+                               textInput("host", "Host", "localhost"),
+                               textInput("dbname", "Database Name", "rmysql"),
+                               textInput("user", "User", "root"),
+                               textInput("password", "Password", ""),
+                               textInput("table", "Table Name", "train_data")
+
+                             )),
+                      column(9,
+                             DT::dataTableOutput("mytable")
+                      )
+
+             )),
     # Data Exploration --- (summary, visualization)
     navbarMenu("Data Exploration", icon = icon("magic"),
                tabPanel("Descriptive Statistics",
@@ -21,10 +44,10 @@ ui <- tagList(
                tabPanel("Data Visualization",
                         icon = icon("bar-chart"))),
     # Data Preprocessing ---
-    tabPanel("Preprocessing", icon = icon("wrench")),
+    tabPanel("Preprocessing", icon = icon("graduation-cap")),
     tabPanel("Setting", icon = icon("cog")),
     tabPanel("Models", icon = icon("folder-open")),
-    navbarMenu("Train and Tuning", icon = icon("graduation-cap"),
+    navbarMenu("Train and Tuning", icon = icon("wrench"),
                tabPanel("Train",
                         icon = icon("graduation-cap")),
                tabPanel("Tuning",
@@ -37,7 +60,7 @@ ui <- tagList(
 ),
 
 # App footer ----
-tags$footer("© 2020 Cuong Sai, Maxim Shcherbakov (Volgograd State Technical University)", align = "left", style = "
+tags$footer("© 2019 Cuong Sai, Maxim Shcherbakov (Volgograd State Technical University)", align = "left", style = "
               position:absolute;
               bottom:0;
               width:100%;
