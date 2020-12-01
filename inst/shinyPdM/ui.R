88bed00f456e1d4b8b5aeca949bf0af56002db05
forvis <cuong.edu3012@gmail.com>
2020-12-01 12:02:23 +0400

    commit old ui.R

--- inst/shinyPdM/ui.R
+++ inst/shinyPdM/ui.R
@@ -1,32 +1,47 @@
 library(shiny)
+library(shiny)
+library(shinythemes)
+library(ggplot2)
+library(caret)
+
 
 # Define UI for app that draws a histogram ----
-ui <- fluidPage(
+ui <- tagList(
+  fluidPage(theme = shinytheme("cerulean"),
 
   # App title ----
-  titlePanel("Hello Shiny!"),
+  titlePanel("PdM: A framework for predictive maintenance in industry 4.0 concept"),
+  navbarPage("PdM",
+    tabPanel("Data Import", icon = icon("folder-open")),
+    navbarMenu("Data Exploration", icon = icon("magic"),
+               tabPanel("Descriptive Statistics",
+                        icon = icon("list-alt")),
+               tabPanel("Data Visualization",
+                        icon = icon("bar-chart"))),
+    tabPanel("Preprocessing", icon = icon("wrench")),
+    tabPanel("Setting", icon = icon("cog")),
+    tabPanel("Models", icon = icon("folder-open")),
+    navbarMenu("Train and Tuning", icon = icon("graduation-cap"),
+               tabPanel("Train",
+                        icon = icon("graduation-cap")),
+               tabPanel("Tuning",
+                        icon = icon("cog"))),
+    tabPanel("Predict", icon = icon("flag")),
+    tabPanel(title = "Github", icon = icon("github", "fa-lg"),
+             value = "https://github.com/forvis/PdM")
 
-  # Sidebar layout with input and output definitions ----
-  sidebarLayout(
+ )
+),
+tags$footer("© 2020 Cuong Sai, Maxim Shcherbakov (Volgograd State Technical University)", align = "left", style = "
+              position:absolute;
+              bottom:0;
+              width:100%;
+              height:50px;   /* Height of the footer */
+              color: white;
+              padding: 10px;
+              background-color: #FF6600;
+              z-index: 1000;")
 
-    # Sidebar panel for inputs ----
-    sidebarPanel(
-
-      # Input: Slider for the number of bins ----
-      sliderInput(inputId = "bins",
-                  label = "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-
-    ),
-
-    # Main panel for displaying outputs ----
-    mainPanel(
-
-      # Output: Histogram ----
-      plotOutput(outputId = "distPlot")
-
-    )
-  )
 )
+
+
